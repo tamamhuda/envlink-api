68c159adafdf36447ac289059e49d93feafeb473
Tama <tamamhuda11@gmail.com>
2025-07-03 23:10:26 +0700

    project structure & initialization

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return json with value status OK', () => {
+      expect(appController.healthz()).toBe({status: 'OK'});
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AccountModule } from './account/account.module';
+import { SessionModule } from './session/session.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [AccountModule, SessionModule, AuthModule, UserModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
